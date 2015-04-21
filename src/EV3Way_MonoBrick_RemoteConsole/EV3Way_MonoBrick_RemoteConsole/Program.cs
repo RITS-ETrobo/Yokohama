6eab59ae5c066f08ce48c3f7cc3bb6f05252cc3e
Geroshabu <oonstks@gmail.com>
2015-04-21 18:58:35 +0900

    refs #36 コンソール側コードを, キー入力待ちしないようにした
    Console.ReadKey()をするとキー入力待ちになり, キーを入力しないとその後のテキスト受信コードに進まない.
    そのため, Console.ReadKey() の前に Console.KeyAvailable を見てキー入力待ちを回避するようにした.

--- src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Program.cs
+++ src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace EV3Way_MonoBrick_RemoteConsole
 {
@@ -31,25 +32,29 @@ namespace EV3Way_MonoBrick_RemoteConsole
 
 			bool	done = false;
 			while (!done) {
-				Console.Out.Write(">");
-				ConsoleKeyInfo key = Console.ReadKey();
-				switch (key.KeyChar) {
-				case 'g':
-				case 's':
-					// LeJOS 版に合わせてネットワークバイトオーダーで送信
-					byte[] keyBytes = BitConverter.GetBytes((int)key.KeyChar); // 4ByteArray (littele Endian)
-					if (BitConverter.IsLittleEndian) {
-						Array.Reverse(keyBytes); // little Endian -> big endian
+				if (Console.KeyAvailable) {
+					Console.Out.Write (">");
+					ConsoleKeyInfo key = Console.ReadKey ();
+					switch (key.KeyChar) {
+					case 'g':
+					case 's':
+						// LeJOS 版に合わせてネットワークバイトオーダーで送信
+						byte[] keyBytes = BitConverter.GetBytes ((int)key.KeyChar); // 4ByteArray (littele Endian)
+						if (BitConverter.IsLittleEndian) {
+							Array.Reverse (keyBytes); // little Endian -> big endian
+						}
+						connection.Write (keyBytes, 0, keyBytes.Length);
+						Console.Out.WriteLine ();
+						break;
+					case 'q':
+						done = true;
+						break;
 					}
-					connection.Write(keyBytes, 0, keyBytes.Length);
-					Console.Out.WriteLine();
-					break;
-				case 'q':
-					done = true;
-					break;
 				}
 
 				RemoteReceiveTest (connection);
+
+				Thread.Sleep (9); // 適当
 			}
 		}
 
