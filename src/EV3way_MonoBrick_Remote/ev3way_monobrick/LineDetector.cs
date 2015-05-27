652a9359aae3b135ebb2c0d6ee716b4ac19ce4e8
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2015-05-27 18:31:33 +0900

    refs #105 Added class of LineDetector and PLineDetector
    ライントレース用のクラスを追加しました。未設計なので、今後設計しなおす必要があります。
    まずは、このクラスを使って、ライントレースの技術調査を行います。

---
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/LineDetector.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ETRobocon.EV3
+{
+	/// <summary>
+	/// 線を検知して、左右の回転係数(どちらにどれくらいの速度で回転すればよいか)を生成するクラスです。
+	/// これは、一番シンプルなもので、黒を検知したら
+	/// </summary>
+	public class LineDetector
+	{
+		public enum LineEdge:sbyte
+		{
+			Left = -1,
+			Right = 1
+		}
+
+		/// ロボットがコース上の白色を検知したときのカラーセンサの値
+		protected int White;
+
+		/// ロボットがコース上の黒色を検知した時のカラーセンサの値
+		protected int Black;
+
+		/// 線のどちらの端をトレースするか
+		protected LineEdge edge;
+
+		/// Turn値の最大/最小値の絶対値
+		protected sbyte MaxTurnAbs;
+
+		public LineDetector( int White, int Black, LineEdge edge, sbyte MaxTurnAbs )
+		{
+			this.White = White;
+			this.Black = Black;
+			this.edge = edge;
+			this.MaxTurnAbs = MaxTurnAbs;
+		}
+
+		/// <summary>
+		/// Turn値を計算します。
+		/// Turn値はBalancer.control()に入力する回転係数のことをいいます.
+		/// </summary>
+		/// <returns>Turn値</returns>
+		public virtual sbyte CalculateTurn( int colorValue )
+		{
+			return (colorValue >= ((Black + White) / 2)) ? (sbyte)(MaxTurnAbs * (sbyte)edge) : (sbyte)(-MaxTurnAbs * (sbyte)edge);
+		}
+
+	}
+
+	/// <summary>
+	/// P(Proposal)比例制御をするLineDetectorです。
+	/// </summary>
+	public class PLineDetector : LineDetector
+	{
+		public PLineDetector ( int White, int Black, LineEdge edge, sbyte MaxTurnAbs)
+			: base (White, Black, edge, MaxTurnAbs)
+		{
+			// nothing to do
+		}
+
+		/// <summary>
+		/// Turn値を計算します。
+		/// </summary>
+		/// <returns>Turn値</returns>
+		public override sbyte CalculateTurn( int colorValue )
+		{
+			return (sbyte)((-((2 * MaxTurnAbs) / (White + Black)) * colorValue + MaxTurnAbs) * (sbyte)edge);
+		}
+	}
+}
+
