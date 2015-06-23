dce2026ec9998b34821d83e20611404bb7d160e2
Geroshabu <oonstks@gmail.com>
2015-06-23 20:34:33 +0900

    refs #61 Added LogTask class files

---
+++ src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Utils/LogTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EV3Way_MonoBrick_RemoteConsole.Utils
+{
+	/// <summary>ログ機能(受信と表示)</summary>
+	public class LogTask
+	{
+		public LogTask ()
+		{
+		}
+	}
+}
+
---
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Utils/LogTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ETRobocon.Utils
+{
+	/// <summary>ログ機能(送信)</summary>
+	public class LogTask
+	{
+		public LogTask ()
+		{
+		}
+	}
+}
+
