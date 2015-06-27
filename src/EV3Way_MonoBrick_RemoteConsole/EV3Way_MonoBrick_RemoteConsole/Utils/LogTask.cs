11a636e561a2941174cd00d7e79c37c34052d5c4
Geroshabu <oonstks@gmail.com>
2015-06-27 16:13:08 +0900

    refs #61 Displayed a message if an error occurs

--- src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Utils/LogTask.cs
+++ src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Utils/LogTask.cs
@@ -26,11 +26,18 @@ namespace EV3Way_MonoBrick_RemoteConsole.Utils
 		/// <summary>ログ受信・表示タスクを開始する</summary>
 		public static void Run()
 		{
-			ProtocolProcessorForPC.Connect();
+			try
+			{
+				ProtocolProcessorForPC.Connect();
 
-			LogTask._instance._logThread = new Thread (LogTask._instance.Loop);
-			LogTask._instance._logThread.Priority = ThreadPriority.Lowest;
-			LogTask._instance._logThread.Start ();
+				LogTask._instance._logThread = new Thread(LogTask._instance.Loop);
+				LogTask._instance._logThread.Priority = ThreadPriority.Lowest;
+				LogTask._instance._logThread.Start();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
 		}
 
 		/// <summary>タスクのメインループ</summary>
@@ -40,47 +47,54 @@ namespace EV3Way_MonoBrick_RemoteConsole.Utils
 
 			while (true)
 			{
-				if (ProtocolProcessorForPC.Instance.ReceiveData(out data))
+				try
 				{
-					// 受信したログを表示
-					if (data is string)	// 文字列
+					if (ProtocolProcessorForPC.Instance.ReceiveData(out data))
 					{
-						Console.WriteLine((string)data);
-					}
-					else if (data is Array)	// それ以外の組み込み型
-					{
-						Array array = (Array)data;
-						string str = "";
-						foreach (object obj in array)
+						// 受信したログを表示
+						if (data is string)	// 文字列
 						{
-							if (obj is bool) { str += ((bool)obj).ToString(); }
-							else if (obj is sbyte) { str += ((sbyte)obj).ToString(); }
-							else if (obj is short) { str += ((short)obj).ToString(); }
-							else if (obj is int) { str += ((int)obj).ToString(); }
-							else if (obj is long) { str += ((long)obj).ToString(); }
-							else if (obj is byte) { str += ((byte)obj).ToString(); }
-							else if (obj is ushort) { str += ((ushort)obj).ToString(); }
-							else if (obj is uint) { str += ((uint)obj).ToString(); }
-							else if (obj is ulong) { str += ((ulong)obj).ToString(); }
-							else if (obj is decimal) { str += ((decimal)obj).ToString(); }
-							else if (obj is char) { str += ((char)obj).ToString(); }
-							else if (obj is float) { str += ((float)obj).ToString(); }
-							else if (obj is double) { str += ((double)obj).ToString(); }
-							else { str += INVALID_RECEIVE_DATA; }
-
-							str += " ";
+							Console.WriteLine((string)data);
 						}
-						Console.WriteLine(str);
-					}
-					else
-					{
-						Console.WriteLine(INVALID_RECEIVE_DATA);
+						else if (data is Array)	// それ以外の組み込み型
+						{
+							Array array = (Array)data;
+							string str = "";
+							foreach (object obj in array)
+							{
+								if (obj is bool) { str += ((bool)obj).ToString(); }
+								else if (obj is sbyte) { str += ((sbyte)obj).ToString(); }
+								else if (obj is short) { str += ((short)obj).ToString(); }
+								else if (obj is int) { str += ((int)obj).ToString(); }
+								else if (obj is long) { str += ((long)obj).ToString(); }
+								else if (obj is byte) { str += ((byte)obj).ToString(); }
+								else if (obj is ushort) { str += ((ushort)obj).ToString(); }
+								else if (obj is uint) { str += ((uint)obj).ToString(); }
+								else if (obj is ulong) { str += ((ulong)obj).ToString(); }
+								else if (obj is decimal) { str += ((decimal)obj).ToString(); }
+								else if (obj is char) { str += ((char)obj).ToString(); }
+								else if (obj is float) { str += ((float)obj).ToString(); }
+								else if (obj is double) { str += ((double)obj).ToString(); }
+								else { str += INVALID_RECEIVE_DATA; }
+
+								str += " ";
+							}
+							Console.WriteLine(str);
+						}
+						else
+						{
+							Console.WriteLine(INVALID_RECEIVE_DATA);
+						}
+
+						continue;
 					}
 
-					continue;
+					Thread.Sleep(LOOP_INTERVAL);
 				}
-
-				Thread.Sleep(LOOP_INTERVAL);
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
 			}
 		}
 	}
