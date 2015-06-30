b29b8a266b33e36c80e51580f5181d2237b6a096
Geroshabu <oonstks@gmail.com>
2015-06-30 13:27:25 +0900

    refs #61 Replaced an old log system with a new log system
    今回, PC→EV3のログ送信は作成したが, EV3→PCは作成していない.
    今回作成した通信方法と, 古い通信方法は, 共存不可(あるいは手間がかかる)ため, EV3→PCの通信を一旦無効とする.
    走行の開始/終了は, 本体スイッチで行ってください.

--- src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Program.cs
+++ src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Program.cs
@@ -1,37 +1,27 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+
+using EV3Way_MonoBrick_RemoteConsole.Utils;
 
 namespace EV3Way_MonoBrick_RemoteConsole
 {
 	class MainClass
 	{
-		// constant
-		private const int SOCKET_PORT = 7360;
-
-		/// EV3から送られてくる文字列の最大長.
-		private const int REMOTE_TRANSFER_SIZE_MAX = 255;
-	
 		public static void Main (string[] args)
 		{
 			try {
-				// 指定されたサーバに接続
-				Socket	sock   = new Socket(
-					AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-				sock.Connect("10.0.1.1", SOCKET_PORT);
-	
-				NetworkStream	conn = new NetworkStream(sock, true);
-				SendCommandLoop(conn); // コンソール入力
-				conn.Close();
+				LogTask.Run();
+
+				SendCommandLoop(); // コンソール入力
 			} catch (Exception e) {
 				Console.Out.WriteLine("caught an exception: {0}", e.Message);
 			}
 		}
 
-		private static void SendCommandLoop(NetworkStream connection) {
-			Console.Out.WriteLine ("Please key g[start] s[stop] q[quit]:");
+		private static void SendCommandLoop() {
+//			Console.Out.WriteLine ("Please key g[start] s[stop] q[quit]:");
+			Console.Out.WriteLine("Push EV3 switch for [start][stop]. Or push key q for [quit] :");
 
 			bool	done = false;
 			while (!done) {
@@ -41,13 +31,16 @@ namespace EV3Way_MonoBrick_RemoteConsole
 					switch (key.KeyChar) {
 					case 'g':
 					case 's':
-						// LeJOS 版に合わせてネットワークバイトオーダーで送信
-						byte[] keyBytes = BitConverter.GetBytes ((int)key.KeyChar); // 4ByteArray (littele Endian)
-						if (BitConverter.IsLittleEndian) {
-							Array.Reverse (keyBytes); // little Endian -> big endian
-						}
-						connection.Write (keyBytes, 0, keyBytes.Length);
-						Console.Out.WriteLine ();
+						// PC→EV3はまだできていないため, コメントアウト.
+						// 当分, 本体スイッチでstart/stopしてください.
+
+//						// LeJOS 版に合わせてネットワークバイトオーダーで送信
+//						byte[] keyBytes = BitConverter.GetBytes ((int)key.KeyChar); // 4ByteArray (littele Endian)
+//						if (BitConverter.IsLittleEndian) {
+//							Array.Reverse (keyBytes); // little Endian -> big endian
+//						}
+//						connection.Write (keyBytes, 0, keyBytes.Length);
+//						Console.Out.WriteLine ();
 						break;
 					case 'q':
 						done = true;
@@ -55,46 +48,9 @@ namespace EV3Way_MonoBrick_RemoteConsole
 					}
 				}
 
-				RemoteReceiveTest (connection);
-
 				// ロボット側の倒立振子制御ループが4msで回るので, それより短い値を指定.
 				Thread.Sleep (2); // TODO: 適切な値か検証する
 			}
-		}
-
-		/// <summary>
-		/// Receive a string from EV3.
-		/// </summary>
-		/// <param name="connection">Connection.</param>
-		/// <remarks>
-		/// 通信プロトコルは次の通りである. ASCII文字として受信する.
-		/// <list type="bullet">
-		/// <item>
-		/// <description>1byte目 : 受信した文字列長. 1byteで表せる範囲の制限上, 文字列の最大長は255である.</description>
-		/// </item>
-		/// <item>
-		/// <description>2byte目以降 : 受信文字列を逆順にしたもの. 当メソッド内で, 元の順に直している.</description>
-		/// </item>
-		/// </list>
-		/// </remarks>
-		private static void RemoteReceiveTest(NetworkStream connection)
-		{
-			// 受信
-			try{
-				if (connection.DataAvailable) {
-					var buff = new byte[REMOTE_TRANSFER_SIZE_MAX + 1];
-					connection.Read(buff, 0, 1);
-					int receiveBytes = buff[0];
-					connection.Read(buff, 0, receiveBytes);
-					// ネットワークバイトオーダー(big endian)で受信したため little endian に変換
-					Array.Reverse(buff, 0, receiveBytes); // big endian -> little endian
-
-					Console.Out.WriteLine(System.Text.Encoding.ASCII.GetString(buff, 0, receiveBytes));
-				}
-			}catch(Exception){
-				return;
-			}
-			return;
 		}
 	}
 }
