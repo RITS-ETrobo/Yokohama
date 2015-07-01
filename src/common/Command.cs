2836d44de2a8a4e65677b05c5eb90ef1b7c2573f
Geroshabu <oonstks@gmail.com>
2015-07-01 20:16:51 +0900

    refs #131 Defined properties and fields of common class

--- src/common/Command.cs
+++ src/common/Command.cs
@@ -2,11 +2,22 @@
 
 namespace ETRobocon.Utils
 {
+	/// <summary>コマンド</summary>
 	public class Command
 	{
+		/// <summary>このコマンドのID</summary>
+		public CommandID Id { get; set; }
+
+		/// <summary>このコマンドの第一パラメータの型</summary>
+		/// <value>第一パラメータが無ければ<c>null</c>, それ以外はその型の<see cref="System.Type"/></value>
+		public Type ParameterType1 { get; set; }
+
+		/// <summary>このコマンドの第二パラメータの型</summary>
+		/// <value>第二パラメータが無ければ<c>null</c>, それ以外はその型の<see cref="System.Type"/></value>
+		public Type ParameterType2 { get; set; }
+
 		public Command ()
 		{
 		}
 	}
 }
-
--- src/common/CommandManager.cs
+++ src/common/CommandManager.cs
@@ -2,9 +2,16 @@
 
 namespace ETRobocon.Utils
 {
+	/// <summary>全ての<see cref="Command"/>を管理するクラス</summary>
 	public class CommandManager
 	{
-		public CommandManager ()
+		/// <summary>このクラスのインスタンス</summary>
+		private static CommandManager _Instance = new CommandManager();
+
+		/// <summary>全コマンドリスト</summary>
+		private Command[] _Commands;
+
+		private CommandManager ()
 		{
 		}
 	}
