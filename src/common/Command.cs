b66f666c6a23082fbee20093d0f8f46a3a88b22f
Geroshabu <oonstks@gmail.com>
2015-07-01 20:52:28 +0900

    refs #131 Allowed to assign nothing to properties of a Command instance

--- src/common/Command.cs
+++ src/common/Command.cs
@@ -6,15 +6,15 @@ namespace ETRobocon.Utils
 	public class Command
 	{
 		/// <summary>このコマンドのID</summary>
-		public CommandID Id { get; set; }
+		public CommandID Id { get; private set; }
 
 		/// <summary>このコマンドの第一パラメータの型</summary>
 		/// <value>第一パラメータが無ければ<c>null</c>, それ以外はその型の<see cref="System.Type"/></value>
-		public Type ParameterType1 { get; set; }
+		public Type ParameterType1 { get; private set; }
 
 		/// <summary>このコマンドの第二パラメータの型</summary>
 		/// <value>第二パラメータが無ければ<c>null</c>, それ以外はその型の<see cref="System.Type"/></value>
-		public Type ParameterType2 { get; set; }
+		public Type ParameterType2 { get; private set; }
 
 		/// <summary>コマンドの作成</summary>
 		/// <param name="commandId">このコマンドのID</param>
