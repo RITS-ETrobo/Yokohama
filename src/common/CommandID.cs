33a1f4433ebfb5eb37827f7f6935a1541835e77d
Geroshabu <oonstks@gmail.com>
2015-07-01 19:40:41 +0900

    refs #131 Defined a variety of commands

--- src/common/CommandID.cs
+++ src/common/CommandID.cs
@@ -2,8 +2,15 @@
 
 namespace ETRobocon.Utils
 {
+	/// <summary>コマンドのID</summary>
+	/// <remarks>EV3, PC両者でIDを統一し, 両者の疎通を図る.</remarks>
 	public enum CommandID
 	{
+		Run,	///< 走行開始
+		Stop,	///< 走行停止
+		Log,	///< ログ送信有効/無効
+
+		NumOfCommand
 	}
 }
 
