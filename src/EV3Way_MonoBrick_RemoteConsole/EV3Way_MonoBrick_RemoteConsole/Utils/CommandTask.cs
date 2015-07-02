592c11856c948fad2d43b7d723ad9d02e9c16f90
Geroshabu <oonstks@gmail.com>
2015-07-02 19:03:30 +0900

    refs #131 Created asynchronous threads for the CommandTask

--- src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Utils/CommandTask.cs
+++ src/EV3Way_MonoBrick_RemoteConsole/EV3Way_MonoBrick_RemoteConsole/Utils/CommandTask.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Threading;	// for Thread
+
+using ETRobocon.Utils;	// for ProtocolProcessorForPC
 
 namespace EV3Way_MonoBrick_RemoteConsole.Utils
 {
+	/// <summary>コマンド入力・送信タスク</summary>
 	public class CommandTask
 	{
 		/// <summary>コマンドタスクのインスタンス</summary>
 		private static CommandTask _Instance = new CommandTask();
 
+		/// <summary>コマンドタスクが実行されているスレッド</summary>
+		private Thread _CommandThread;
+
+		/// <summary>コマンドタスクのメインループのSleep時間</summary>
+		private const int LOOP_INTERVAL = 16;
+
 		private CommandTask()
 		{
+		}
+
+		/// <summary>コマンドタスクの開始</summary>
+		public static void Run()
+		{
+			ProtocolProcessorForPC.Connect();
+
+			CommandTask._Instance._CommandThread = new Thread(CommandTask._Instance.Loop);
+			CommandTask._Instance._CommandThread.Priority = ThreadPriority.Lowest;
+			CommandTask._Instance._CommandThread.Start();
+		}
+
+		/// <summary>コマンドタスクのメインループ</summary>
+		private void Loop()
+		{
+			while (true)
+			{
+				Thread.Sleep(LOOP_INTERVAL);
+			}
 		}
 	}
 }
--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Utils/CommandTask.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Utils/CommandTask.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Threading;	// for Thread
 
 namespace ETRobocon.Utils
 {
+	/// <summary>コマンド受信・実行タスク</summary>
 	public class CommandTask
 	{
 		/// <summary>コマンドタスクのインスタンス</summary>
 		private static CommandTask _Instance = new CommandTask();
 
+		/// <summary>コマンドタスクが実行されているスレッド</summary>
+		private Thread _CommandThread;
+
+		/// <summary>コマンドタスクのメインループのSleep時間</summary>
+		private const int LOOP_INTERVAL = 16;
+
 		private CommandTask()
 		{
+		}
+
+		/// <summary>コマンドタスクの開始</summary>
+		public static void Run()
+		{
+			ProtocolProcessorForEV3.Connect();
+
+			CommandTask._Instance._CommandThread = new Thread(CommandTask._Instance.Loop);
+			CommandTask._Instance._CommandThread.Priority = ThreadPriority.Lowest;
+			CommandTask._Instance._CommandThread.Start();
+		}
+
+		/// <summary>コマンドタスクのメインループ</summary>
+		private void Loop()
+		{
+			while (true)
+			{
+				Thread.Sleep(LOOP_INTERVAL);
+			}
 		}
 	}
 }
