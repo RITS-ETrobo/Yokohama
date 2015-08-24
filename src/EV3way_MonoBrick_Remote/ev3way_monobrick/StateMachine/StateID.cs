c3a9291ca4b0d1a6ce7f1c0d78d52a750ad99e58
Geroshabu <oonstks@gmail.com>
2015-08-24 20:26:04 +0900

    refs #214 Renamed elements of the StateID

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/StateID.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/StateID.cs
@@ -5,17 +5,17 @@ namespace ETRobocon.StateMachine
 	public enum StateID
 	{
 		// 走行準備
-		SettingMenu,
+		Ready,
 
 		// 走行
-		LineTraceHighSpeed,
+		Straight1,	///< 最初の直線区間
 
 		// ルックアップゲート用
 
 		// フィギュアL用
 
 		// その他
-		Complete,
+		Complete,	///< 完走 (走行停止)
 
 		NumOfState
 	}
