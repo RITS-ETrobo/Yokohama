1b188a21e4e34c622263e7bff5b32928af4ee5e4
Geroshabu <oonstks@gmail.com>
2015-08-24 21:02:56 +0900

    refs #214 Implemented a mechanism of the state loop

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/StateMachine.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/StateMachine.cs
@@ -4,12 +4,39 @@ namespace ETRobocon.StateMachine
 {
 	public class StateMachine
 	{
+		/// <summary>現在の状態</summary>
+		private StateID currentState;
+
+		/// <summary>状態の実体</summary>
+		private State[] _states;
+
 		public StateMachine ()
 		{
+			currentState = StateID.Ready;
+
+			_states = new State[(int)StateID.NumOfState]
+			{
+				new ReadyState(),	// Ready
+				new StraightWithLineTraceState(),	// Straight1
+				new CompleteState()	// Complete
+			};
+
 		}
 
 		public void Run()
 		{
+			while (true)
+			{
+				_states[(int)currentState].Do();
+				
+				TriggerID trigger = _states[(int)currentState].JudgeTransition();
+
+				if (trigger != TriggerID.NoTrigger)
+				{
+					// トリガー発生
+				}
+
+			}
 		}
 	}
 }
