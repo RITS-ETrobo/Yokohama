592f8ed6bfdb61985343b95518976ec0a92189a3
Geroshabu <oonstks@gmail.com>
2015-08-24 20:38:29 +0900

    refs #214 Added 3 empty states for the prototype

---
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/ReadyState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ETRobocon.StateMachine
+{
+	public class ReadyState : State
+	{
+		public ReadyState()
+		{
+		}
+
+		public override void Enter()
+		{
+		}
+
+		public override void Do()
+		{
+		}
+
+		public override void Exit()
+		{
+		}
+
+		public override TriggerID JudgeTransition()
+		{
+			return TriggerID.NoTrigger;
+		}
+	}
+}
+
