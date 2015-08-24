766a9fba8e23d2cbd55c2177c92a484588e970d5
Geroshabu <oonstks@gmail.com>
2015-08-24 21:55:04 +0900

    refs #214 Implemented a mechanism of the transition of the state

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/StateMachine.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/StateMachine.cs
@@ -10,6 +10,9 @@ namespace ETRobocon.StateMachine
 		/// <summary>状態の実体</summary>
 		private State[] _states;
 
+		/// <summary>状態遷移表</summary>
+		private Transition?[,] TransitionMatrix;
+
 		public StateMachine ()
 		{
 			currentState = StateID.Ready;
@@ -21,6 +24,25 @@ namespace ETRobocon.StateMachine
 				new CompleteState()	// Complete
 			};
 
+			TransitionMatrix = new Transition?[(int)StateID.NumOfState, (int)TriggerID.NumOfTrigger]
+			{
+				// TouchSensor
+
+				// 走行準備
+				{ new Transition(StateID.Straight1, Nop) },
+
+				// ゴールまで走行
+				{ new Transition(StateID.Complete, Nop) },
+
+				// ルックアップゲート用
+
+				// フィギュアL用
+
+				// ...
+
+				// その他
+				{ null }
+			};
 		}
 
 		public void Run()
@@ -34,8 +56,42 @@ namespace ETRobocon.StateMachine
 				if (trigger != TriggerID.NoTrigger)
 				{
 					// トリガー発生
+
+					Transition? transition = TransitionMatrix[(int)currentState, (int)trigger];
+					if (transition.HasValue)
+					{
+						// 状態遷移表に該当する遷移があれば, 遷移させる
+
+						transition.Value.TransitionMethod ();
+						currentState = transition.Value.NextState;
+					}
 				}
 
+			}
+		}
+
+		private void Nop()
+		{
+			// Nothing to do
+		}
+
+		/// <summary>遷移メソッドのDelegate</summary>
+		public delegate void TransitionMethodDel();
+
+		/// <summary>遷移</summary>
+		/// <remarks>次のStateと, 遷移メソッドの組</remarks>
+		public struct Transition
+		{
+			/// <summary>次のStateのStateID</summary>
+			public StateID NextState;
+			/// <summary>遷移メソッド</summary>
+			public TransitionMethodDel TransitionMethod;
+
+			/// <summary>コンストラクタ</summary>
+			public Transition(StateID nextState, TransitionMethodDel transitionMethod)
+			{
+				NextState = nextState;
+				TransitionMethod = transitionMethod;
 			}
 		}
 	}
