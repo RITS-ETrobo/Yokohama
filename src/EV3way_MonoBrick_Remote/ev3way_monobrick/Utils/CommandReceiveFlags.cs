7322e49ee3c764da3b7e0593d6605703a426fc1d
Geroshabu <oonstks@gmail.com>
2015-08-26 01:01:24 +0900

    refs #223 Made the CommandReceiveFlags class a singleton

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Utils/CommandReceiveFlags.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Utils/CommandReceiveFlags.cs
@@ -4,6 +4,17 @@ namespace ETRobocon.Utils
 {
 	public class CommandReceiveFlags
 	{
+        /// <summary>Instanceのフィールド</summary>
+        private static CommandReceiveFlags _instance = new CommandReceiveFlags();
+        /// <summary>インスタンスへの参照</summary>
+        public static CommandReceiveFlags Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
 		public CommandReceiveFlags()
 		{
 		}
