2dc98ae4dae0a7fb46701de8e1bd31cb896460d3
Geroshabu <oonstks@gmail.com>
2015-08-26 01:04:27 +0900

    refs #223 Added flag fields

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Utils/CommandReceiveFlags.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Utils/CommandReceiveFlags.cs
@@ -15,8 +15,18 @@ namespace ETRobocon.Utils
             }
         }
 
+        /// <summary>コマンド受信フラグ</summary>
+        /// <remarks>各要素は, <see cref="CommandID"/>の定義順と一致する.</remarks>
+        private bool[] _flags;
+
 		public CommandReceiveFlags()
 		{
+            _flags = new bool[(int)CommandID.NumOfCommand];
+            // 一応, 明示的に初期化
+            for (int i = 0; i < _flags.Length; i++)
+            {
+                _flags[i] = false;
+            }
 		}
 	}
 }
