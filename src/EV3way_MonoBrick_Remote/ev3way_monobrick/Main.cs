1f107310ae24b6d81393b6241fae699458ee3b5b
Geroshabu <oonstks@gmail.com>
2015-08-26 21:09:05 +0900

    refs #214 Switched the flow to the StateMachine flow

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Main.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Main.cs
@@ -7,6 +7,7 @@ using MonoBrickFirmware.Display;
 
 using ETRobocon.EV3;
 using ETRobocon.Utils;
+using ETRobocon.StateMachine;
 
 ///	2輪倒立振子ライントレースロボットの MonoBrick 用 c# プログラム
 namespace ETRobocon.EV3
@@ -65,6 +66,11 @@ namespace ETRobocon.EV3
 			// 構造体の宣言と初期化
 			var body = new EV3body ();
 			EV3body.init (ref body);
+
+			var state_machine = new StateMachine.StateMachine(body);
+			state_machine.Run();
+
+			// これ以降は実行されない
 
 			// Bluetooth関係のETロボコン拡張機能を有効にする
 			Brick.InstallETRoboExt ();
