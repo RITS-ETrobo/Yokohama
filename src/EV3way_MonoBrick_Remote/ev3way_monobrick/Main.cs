0189d701f4c4d79358283c583c6b026e696bc40c
masjinno <z00s600013@ZRFG031564.rfg.rfgad.ricoh.com>
2015-08-27 19:51:42 +0900

    refs #81 Modified MotorTail class.

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Main.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Main.cs
@@ -132,9 +132,9 @@ namespace ETRobocon.EV3
 
 			RemoteLogTest ("EV3 is ready.", connection);
 
-			body.motorTail.SetMotorAngle (MotorTail.TAIL_ANGLE_STAND_UP);	//完全停止用角度に制御
-
+			body.motorTail.SetMotorAngleImmediately (MotorTail.TAIL_ANGLE_STAND_UP);	//完全停止用角度に制御
 			while (!body.touch.IsPressed()) {
+				body.motorTail.UpdateTailAngle ();
 				if (checkRemoteCommand(connection, REMOTE_COMMAND_START)) {
 					break;  // PC で 'g' キーが押された
 				}
@@ -142,8 +142,9 @@ namespace ETRobocon.EV3
 				Thread.Sleep (4);
 			}
 
+			body.motorTail.SetMotorAngleImmediately (MotorTail.TAIL_ANGLE_STAND_UP);	//完全停止用角度に制御
 			while (body.touch.IsPressed ()) {
-				body.motorTail.SetMotorAngle (MotorTail.TAIL_ANGLE_STAND_UP);	//完全停止用角度に制御
+				body.motorTail.UpdateTailAngle ();
 				Thread.Sleep (4);
 			}
 		}
@@ -159,44 +160,45 @@ namespace ETRobocon.EV3
 
 			RemoteLogTest ("EV3 run.", connection);
 
-			body.motorTail.SetMotorAngle(MotorTail.TAIL_ANGLE_DRIVE);	// バランス走行用角度に制御
+			body.motorTail.SetMotorAngleSlowly(MotorTail.TAIL_ANGLE_LOOKUPGATE, 100);	// バランス走行用角度に制御
 			while (!body.touch.IsPressed ()) 
 			{
+				body.motorTail.UpdateTailAngle ();
 				if (checkRemoteCommand(connection, REMOTE_COMMAND_STOP)) {
 					break; // PC で 's' キー押されたら走行終了
 				}
 
-				if (++counter >= 40/4) {
-					alert = sonar_alert (body);
-					counter = 0;
-				}
-				if (alert) {
-					forward = 0;
-					turn = 0;
-				} else {
-					forward = 50;
-					turn = (body.color.Read () >= (LIGHT_BLACK + LIGHT_WHITE) / 2) ? (sbyte)50 : (sbyte)-50;
-				}
-
-				int gyroNow = -body.gyro.Read();
-				int thetaL = body.motorL.GetTachoCount();
-				int theTaR = body.motorR.GetTachoCount();
-				sbyte pwmL, pwmR;
-				Balancer.control (
-					(float)forward, (float)turn, (float)gyroNow, (float)GYRO_OFFSET, (float)thetaL, (float)theTaR, (float)battery,
-					out pwmL, out pwmR
-				);
-
-				if (pwmL == 0) {
-					body.motorL.Brake();
-				} else {
-					body.motorL.SetPower(pwmL);
-				}
-				if (pwmR == 0) {
-					body.motorR.Brake();
-				} else {
-					body.motorR.SetPower(pwmR);
-				}
+//				if (++counter >= 40/4) {
+//					alert = sonar_alert (body);
+//					counter = 0;
+//				}
+//				if (alert) {
+//					forward = 0;
+//					turn = 0;
+//				} else {
+//					forward = 50;
+//					turn = (body.color.Read () >= (LIGHT_BLACK + LIGHT_WHITE) / 2) ? (sbyte)50 : (sbyte)-50;
+//				}
+//
+//				int gyroNow = -body.gyro.Read();
+//				int thetaL = body.motorL.GetTachoCount();
+//				int theTaR = body.motorR.GetTachoCount();
+//				sbyte pwmL, pwmR;
+//				Balancer.control (
+//					(float)forward, (float)turn, (float)gyroNow, (float)GYRO_OFFSET, (float)thetaL, (float)theTaR, (float)battery,
+//					out pwmL, out pwmR
+//				);
+//
+//				if (pwmL == 0) {
+//					body.motorL.Brake();
+//				} else {
+//					body.motorL.SetPower(pwmL);
+//				}
+//				if (pwmR == 0) {
+//					body.motorR.Brake();
+//				} else {
+//					body.motorR.SetPower(pwmR);
+//				}
 
 				// バランス制御のみだと3msecで安定
 				// 尻尾制御と障害物検知を使用する場合2msecで安定
--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Motor/MotorTail.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Motor/MotorTail.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using MonoBrickFirmware.Movement;
+﻿using MonoBrickFirmware.Movement;
 
 using System.Threading;		// use WaitHandle
+
+using MonoBrickFirmware.Display;
 
 namespace ETRobocon.EV3
 {
@@ -9,10 +10,10 @@ namespace ETRobocon.EV3
 	public class MotorTail
 	{
 
-		public const int TAIL_ANGLE_STAND_UP = 91;   //完全停止時の角度[deg]
+		public const int TAIL_ANGLE_STAND_UP = 95;   //完全停止時の角度[deg]
 		public const int TAIL_ANGLE_DRIVE = 5;       //バランス走行時の角度[deg]
-		public const int TAIL_ANGLE_LOOKUPGATE = 42; //ルックアップゲート用角度. 調整済み.
-		private const int TAIL_ANGLE_MAX = 150;		 //尻尾の角度の最大. この角度を超えた制御はこの角度に収められる.
+		public const int TAIL_ANGLE_LOOKUPGATE = 70; //ルックアップゲート用角度[deg]. 調整済み.
+		private const int TAIL_ANGLE_MAX = 150;		 //尻尾の角度の最大. この角度を上回る制御はこの角度に収められる.
 		private const int TAIL_ANGLE_MIN = 0;		 //尻尾の角度の最小. この角度を下回る制御はこの角度に収められる
 
 		///	完全停止用モータ制御比例係数
@@ -25,78 +26,131 @@ namespace ETRobocon.EV3
 		private Motor motorTail;
 
 		/// <summary>尻尾モーターの, 上がりきった位置からの角度[deg]. </summary>
-		public int Angle { private set; get; }
+		//public int Angle { private set; get; }
+
+		/// <summary>SetMotorAngleImmediately(もしくはSetMotorAngleSlowly)で指定したときの角度</summary>
+		private int startAngle;
+
+		/// <summary>最終的に一致させる角度</summary>
+		private int targetAngle;
+
+		/// <summary>段階的に動かすときに, 目標とする角度</summary>
+		private int subTargetAngle;
+
+		/// <summary>1段階で動かす角度</summary>
+		private float subTargetAnglePerStep;
+
+		/// <summary>targetAngleに持っていくまでの段階数</summary>
+		private int movingSteps;
+
+		/// <summary>現在の段階/movingSteps</summary>
+		private int currentStep;
 
 		/// <summary>Create instance</summary>
 		public MotorTail ()
 		{
-			this.motorTail = new Motor (MotorPort.OutA);
+			motorTail = new Motor (MotorPort.OutA);
 
 			// 初回アクセスすることで, staticメンバの準備に時間がかかるのを防げる(らしい).
-			this.motorTail.SetPower (0);
-			this.motorTail.ResetTacho ();
+			motorTail.SetPower (0);
+			motorTail.ResetTacho ();
+
+			int count_resettailtacho = 100;
+			while (count_resettailtacho > 0) {
+				motorTail.SetPower (-5);
+				count_resettailtacho--;
+				Thread.Sleep (4);
+			}
+			motorTail.Brake ();
+			motorTail.ResetTacho ();
 		}
 
+		~MotorTail(){
+			TurnOff ();
+		}
 
 		///<summary>Turn the motor off.</summary>
 		public void TurnOff()
 		{
-			this.motorTail.Off ();
+			motorTail.Off ();
 		}
 
 		/// <summary>
 		/// 尻尾モーターの角度を設定する.
 		/// </summary>
 		/// <param name="angle">設定したい角度</param>
-		public void SetMotorAngle(int angle)
+		public void SetMotorAngleImmediately(int angle)
 		{
-			WaitHandle motorWaitHandle;
-			Int32 startTachoCount;
-			Int32 moveTachoCount;
-			sbyte speed;
-
 			if (angle > TAIL_ANGLE_MAX) {
 				angle = TAIL_ANGLE_MAX;
 			}
 			if (angle < TAIL_ANGLE_MIN) {
 				angle = TAIL_ANGLE_MIN;
 			}
-			Angle = angle + 3;	// クラスで定めている角度からAPIが定めている角度に変換
+			startAngle = motorTail.GetTachoCount ();
+			targetAngle = angle;
+			movingSteps = 1;
+			currentStep = 1;
+			subTargetAnglePerStep = targetAngle - startAngle;
+			subTargetAngle = targetAngle;
+		}
 
-			startTachoCount = motorTail.GetTachoCount ();
-			moveTachoCount = Angle - startTachoCount;
-
-			if (moveTachoCount > 0) {
-				speed = 100;
-			} else {
-				speed = -100;
-				moveTachoCount = 0 - moveTachoCount;
+		/// <summary>
+		/// 尻尾モーターの角度を指定した段階数を踏んで設定する.
+		/// </summary>
+		/// <param name="angle">設定したい角度</param>
+		/// <param name="steps">段階数. 値が大きいほどゆっくり設定する</param>
+		public void SetMotorAngleSlowly(int angle, int steps)
+		{
+			if (angle > TAIL_ANGLE_MAX) {
+				angle = TAIL_ANGLE_MAX;
 			}
-
-			if (moveTachoCount == 0) {
-				return;
+			if (angle < TAIL_ANGLE_MIN) {
+				angle = TAIL_ANGLE_MIN;
 			}
+			startAngle = motorTail.GetTachoCount ();
+			targetAngle = angle;
+			movingSteps = steps;
+			currentStep = 1;
+			subTargetAnglePerStep = (float)(targetAngle - startAngle) / movingSteps;
+			subTargetAngle = (int)(startAngle + subTargetAnglePerStep * currentStep);
+		}
 
-			/*motorWaitHandle = */motorTail.SpeedProfile (speed, 0, (uint)moveTachoCount, 0, true);
-			//motorWaitHandle.WaitOne();
 
-			/* これまでの尻尾制御ソースコード
-			 * ループ(4ms周期?)の中で常に呼び出し, その呼び出しのときの回転数(角度)から計算したパワーを
-			 * モーターに送ることで制御している.
-			
-			float pwm = (float)(Angle - this.motorTail.GetTachoCount ()) * P_GAIN; // 比例制御
+		/// <summary>
+		/// This function is called in main loop.
+		/// This function uses some parameters which were set by setMotorAngleImmediately() or setMotorAngleSlowly().
+		/// So, each of these 2 functions must be called only once before UpdateTailAngle().
+		/// </summary>
+		public void UpdateTailAngle(){
+			float pwm;
+			int currentTacho;
+			currentTacho = motorTail.GetTachoCount ();
+
+			if (currentStep > movingSteps) {
+				pwm = (targetAngle - currentTacho)*P_GAIN;
+			}else{
+				// 尻尾が段階的目標角度になっていたら, 次の目標角度に更新
+				if (currentTacho == subTargetAngle) {
+					currentStep++;
+					subTargetAngle = (int)(startAngle + subTargetAnglePerStep * currentStep);
+				}
+				// モーターに送るパワーを決定する.
+				pwm = (subTargetAngle - currentTacho) * P_GAIN; // 比例制御
+			}
 			// PWM出力飽和処理
 			if (pwm > PWM_ABS_MAX) {
 				pwm = PWM_ABS_MAX;
 			} else if (pwm < -PWM_ABS_MAX) {
 				pwm = -PWM_ABS_MAX;
 			}
+
+			// 尻尾のモーターに送る動力を設定. 
 			if ((sbyte)pwm == 0) {
-				this.motorTail.Brake();
+				motorTail.Brake();
 			} else {
-				this.motorTail.SetPower((sbyte)pwm);
+				motorTail.SetPower((sbyte)pwm);
 			}
-			*/
 		}
 	}
 }
