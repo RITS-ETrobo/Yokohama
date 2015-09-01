8b6a2af61173e8fa19a47fed6b7c37019f68a7c3
Geroshabu <oonstks@gmail.com>
2015-09-01 21:58:13 +0900

    refs #230 Implemented an asynchronous Show()

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Body/NonModalSelectDialog.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Body/NonModalSelectDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using MonoBrickFirmware.Display.Dialogs;
 
 namespace ETRobocon.Body
@@ -13,10 +14,28 @@ namespace ETRobocon.Body
 		/// <value>表示されているときは<c>true</c>, 表示されていないときは<c>false</c>.</value>
 		public bool IsShowing { get; set; }
 
+		/// <summary>モーダルダイアログを非モーダルとして使うための非同期タスク</summary>
+		private Task _task;
+
 		public NonModalSelectDialog(SelectionType[] selections, string title, bool allowEsc)
 			: base(selections, title, allowEsc)
 		{
 			IsShowing = false;
+		}
+
+		/// <summary>この非モーダル選択ダイアログを表示する</summary>
+		/// <returns>成功時 : true, 既に表示されていた場合 : false</returns>
+		public new bool Show()
+		{
+			_task = new Task(this.Run);
+			_task.Start();
+
+			return true;
+		}
+
+		private void Run()
+		{
+			base.Show();
 		}
 	}
 }
