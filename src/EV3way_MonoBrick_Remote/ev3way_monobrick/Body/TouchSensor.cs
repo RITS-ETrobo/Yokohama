fc0486e9b2712d9ab3e894ba417b1f66a0d943cd
Geroshabu <oonstks@gmail.com>
2015-09-07 21:21:11 +0900

    refs #232 Added a field of a wrapped touch sensor

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Body/TouchSensor.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Body/TouchSensor.cs
@@ -1,11 +1,16 @@
 using System;
+using MonoBrickFirmware.Sensors;
 
 namespace ETRobocon.Body
 {
 	public class TouchSensor
 	{
-		public TouchSensor ()
+		/// <summary>既存のタッチセンサクラス</summary>
+		private EV3TouchSensor _ev3TouchSensor;
+
+		public TouchSensor()
 		{
+			_ev3TouchSensor = new EV3TouchSensor(SensorPort.In1);
 		}
 	}
 }
