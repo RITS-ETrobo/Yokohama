e27a97c976b6656c808a68712da208a310d02f38
Geroshabu <oonstks@gmail.com>
2015-09-07 21:28:24 +0900

    refs #232 Added a comment for the TouchSensor class

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/Body/TouchSensor.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Body/TouchSensor.cs
@@ -3,6 +3,10 @@ using MonoBrickFirmware.Sensors;
 
 namespace ETRobocon.Body
 {
+	/// <summary>
+	/// MonoBrickFirmware.Sensors.EV3TouchSensor をラップし,
+	/// タッチセンサ状態が変化したことを検知できるようにしたクラス.
+	/// </summary>
 	public class TouchSensor
 	{
 		/// <summary>既存のタッチセンサクラス</summary>
