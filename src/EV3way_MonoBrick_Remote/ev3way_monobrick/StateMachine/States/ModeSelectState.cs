d9099aedf0c1379634641f8fd0f9793eebf7dbc7
Geroshabu <oonstks@gmail.com>
2015-09-10 21:07:49 +0900

    refs #229 Implements the empty state

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/ModeSelectState.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/ModeSelectState.cs
@@ -1,12 +1,30 @@
 using System;
+using ETRobocon.EV3;
+using ETRobocon.Utils;
 
 namespace ETRobocon.StateMachine
 {
-	public class ModeSelectState
+	public class ModeSelectState : State
 	{
-		public ModeSelectState ()
+		public ModeSelectState(EV3body body) : base(body, 4)
 		{
+		}
+
+		public override void Enter()
+		{
+		}
+
+		public override void Do()
+		{
+		}
+
+		public override void Exit()
+		{
+		}
+
+		public override TriggerID JudgeTransition()
+		{
+			return TriggerID.NoTrigger;
 		}
 	}
 }
-
