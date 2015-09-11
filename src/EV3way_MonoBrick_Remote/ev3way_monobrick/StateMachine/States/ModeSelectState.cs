614b01fadccebbe57ee19ecc5a7ee1aa2b437efa
Geroshabu <oonstks@gmail.com>
2015-09-11 19:30:02 +0900

    refs #229 Added TODO comments

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/ModeSelectState.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/ModeSelectState.cs
@@ -12,18 +12,24 @@ namespace ETRobocon.StateMachine
 
 		public override void Enter()
 		{
+			// TODO: show a modeless selection dialog
 		}
 
 		public override void Do()
 		{
+			// Nothing to do
 		}
 
 		public override void Exit()
 		{
+			// Nothing to do
 		}
 
 		public override TriggerID JudgeTransition()
 		{
+			// TODO: return trigger for touch sensor
+			// TODO: return trigger for the selection
+
 			return TriggerID.NoTrigger;
 		}
 	}
