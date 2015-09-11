e1b65ae288dbc3009b7c56028e697a72a10ae958
Geroshabu <oonstks@gmail.com>
2015-09-11 19:23:52 +0900

    refs #295 Prepared selection dialog parameters

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/CalibrationModeState.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/CalibrationModeState.cs
@@ -5,8 +5,23 @@ namespace ETRobocon.StateMachine
 {
 	public class CalibrationModeState : State
 	{
+		/// <summary>キャリブレーションステートで表示される選択ダイアログの選択肢</summary>
+		private string[] _selections;
+
+		/// <summary>キャリブレーションステートで表示される選択ダイアログのタイトル</summary>
+		private string _dialogTitle;
+
 		public CalibrationModeState(EV3body body) : base(body, 4)
 		{
+			_selections = new string[]
+			{
+				"Calibrate White",
+				"Calibrate Black",
+				"Calibrate Gray",
+				"Back"
+			};
+
+			_dialogTitle = "Calibration Mode";
 		}
 
 		public override void Enter()
