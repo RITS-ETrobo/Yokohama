643d9a78545af6848e9e1a3bcdecd1e2329390c0
masjinno <z00s600013@ZRFG031564.rfg.rfgad.ricoh.com>
2015-09-12 12:25:42 +0900

    refs #282 Added ColorSensor class and file
    カラーセンサーのクラス・ファイルを追加しました。

---
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/Body/ColorSensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MonoBrickFirmware.Sensors;
+
+namespace ETRobocon.Body
+{
+	/// <summary></summary>
+	public class ColorSensor
+	{
+		/// <summary></summary>
+		private EV3ColorSensor _ev3ColorSensor;
+
+
+
+		/// <summary>カラーセンサーインスタンスの作成</summary>
+		/// <param name="portSensorIn">カラーセンサーに対応する入力ポート</param>
+		public ColorSensor(SensorPort portSensorIn)
+		{
+			_ev3ColorSensor = new EV3ColorSensor(portSensorIn);
+
+		}
+
+	}
+}
+
