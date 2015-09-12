0dc51e7d5a35284f07c33d431e2dbba67f2976e4
Geroshabu <oonstks@gmail.com>
2015-09-12 15:01:17 +0900

    refs #305 Fixed a commit omission

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/StraightWithLineTraceState.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/StraightWithLineTraceState.cs
@@ -12,8 +12,11 @@ namespace ETRobocon.StateMachine
 		private int _counter = 0;
 		private bool _alert = false;
 
+		private LineDetector _ld;
+
 		public StraightWithLineTraceState(EV3body body) : base(body, 2)
 		{
+			_ld = new LineDetectorOld (0, 60, LineDetector.LineEdge.Left, 20.0f, 0, 80.0f ); // TODO: kp, ki, kdの値を変えて調査してください。
 		}
 
 		public override void Enter()
@@ -49,7 +52,7 @@ namespace ETRobocon.StateMachine
 				turn = 0;
 			} else {
 				forward = 50;
-				turn = (_body.color.Read () >= (LIGHT_BLACK + LIGHT_WHITE) / 2) ? (sbyte)50 : (sbyte)-50;
+				turn = _ld.CalculateTurn(_body.color.Read());
 			}
 
 			int gyroNow = _body.gyro.Read();
