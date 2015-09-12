8852a9b9cc932c514db26e16e050682b790e35af
Masanori Ishigami <masanori.ishigami@gmail.com>
2015-09-12 23:58:18 +0900

    refs #309 Modified spaces

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/TriggerID.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/TriggerID.cs
@@ -4,7 +4,7 @@ namespace ETRobocon.StateMachine
 {
 	///	<summary>走行に関するTrigger</summary>
 	///	<remarks>
-	/// Triggerを変更した時は、同時に
+	///	Triggerを変更した時は、同時に
 	///	$\src\EV3way_MonoBrick_Remote\ev3way_monobrick\StateMachine\StateMachine.cs
 	///	の
 	///	TransitionMatrix = new Transition?[(int)StateID.NumOfState, (int)TriggerID.NumOfTrigger]
