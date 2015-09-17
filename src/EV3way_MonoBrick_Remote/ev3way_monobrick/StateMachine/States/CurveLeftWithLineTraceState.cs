14aebcb6c28e5e524826c90d50d0dff7b757d4aa
Geroshabu <oonstks@gmail.com>
2015-09-17 18:32:41 +0900

    refs #312 Implemented a empty state

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/CurveLeftWithLineTraceState.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/CurveLeftWithLineTraceState.cs
@@ -1,12 +1,29 @@
 using System;
+using ETRobocon.EV3;
 
 namespace ETRobocon.StateMachine
 {
-	public class CurveLeftWithLineTraceState
+	public class CurveLeftWithLineTraceState : State
 	{
-		public CurveLeftWithLineTraceState ()
+		public CurveLeftWithLineTraceState(EV3body body) : base(body, 2)
 		{
+		}
+
+		public override void Enter()
+		{
+		}
+
+		public override void Do()
+		{
+		}
+
+		public override void Exit()
+		{
+		}
+
+		public override TriggerID JudgeTransition()
+		{
+			return TriggerID.NoTrigger;
 		}
 	}
 }
-
