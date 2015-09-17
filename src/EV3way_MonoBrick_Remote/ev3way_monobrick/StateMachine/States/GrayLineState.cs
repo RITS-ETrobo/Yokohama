27118b9346fef5c494db65d9793382bf6054495d
Geroshabu <oonstks@gmail.com>
2015-09-17 20:53:09 +0900

    refs #322 Implemented a empty state

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/GrayLineState.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/GrayLineState.cs
@@ -1,12 +1,29 @@
 using System;
+using ETRobocon.EV3;
 
 namespace ETRobocon.StateMachine
 {
-	public class GrayLineState
+	public class GrayLineState : State
 	{
-		public GrayLineState()
+		public GrayLineState(EV3body body) : base(body, 2)
 		{
+		}
+
+		public override void Enter()
+		{
+		}
+
+		public override void Do()
+		{
+		}
+
+		public override void Exit()
+		{
+		}
+
+		public override TriggerID JudgeTransition()
+		{
+			return TriggerID.NoTrigger;
 		}
 	}
 }
-
