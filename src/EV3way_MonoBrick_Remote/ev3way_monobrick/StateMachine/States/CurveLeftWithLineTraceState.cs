a8d12f1ec0960e90d40f5de805e134a1fda22302
Geroshabu <oonstks@gmail.com>
2015-09-17 18:39:58 +0900

    refs #312 Copied similar process from the straight state

--- src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/CurveLeftWithLineTraceState.cs
+++ src/EV3way_MonoBrick_Remote/ev3way_monobrick/StateMachine/States/CurveLeftWithLineTraceState.cs
@@ -1,20 +1,70 @@
 using System;
 using ETRobocon.EV3;
+using ETRobocon.Utils;
 
 namespace ETRobocon.StateMachine
 {
 	public class CurveLeftWithLineTraceState : State
 	{
+		// MainClassのrun()より移してきた
+		private int _batteryLevel;
+		private int _counter = 0;
+		private bool _alert = false;
+
+		private LineDetector _ld;
+
 		public CurveLeftWithLineTraceState(EV3body body) : base(body, 2)
 		{
+			_ld = new LineDetectorOld(0, 60, LineDetector.LineEdge.Left, 20.0f, 0, 80.0f ); // TODO: kp, ki, kdの値を変えて調査してください。
 		}
 
 		public override void Enter()
 		{
+			// 電圧を取得
+			_batteryLevel = Brick.GetVoltageMilliVolt();
 		}
 
 		public override void Do()
 		{
+			sbyte forward;
+			sbyte turn;
+
+			_body.motorTail.UpdateTailAngle ();
+
+			if (++_counter >= 40/4) {
+				_alert = sonar_alert (_body);
+				_counter = 0;
+			}
+			if (_alert) {
+				forward = 0;
+				turn = 0;
+			} else {
+				forward = 50;
+				turn = _ld.CalculateTurn(_body.color.Read());
+			}
+
+			int gyroNow = _body.gyro.GetSensorValue();
+			int thetaL = _body.motorL.GetTachoCount();
+			int theTaR = _body.motorR.GetTachoCount();
+			sbyte pwmL, pwmR;
+			Balancer.control (
+				(float)forward, (float)turn, (float)gyroNow, (float)GYRO_OFFSET, (float)thetaL, (float)theTaR, (float)_batteryLevel,
+				out pwmL, out pwmR
+			);
+
+			if (pwmL == 0) {
+				_body.motorL.Brake();
+			} else {
+				_body.motorL.SetPower(pwmL);
+			}
+			if (pwmR == 0) {
+				_body.motorR.Brake();
+			} else {
+				_body.motorR.SetPower(pwmR);
+			}
+
+			// 自己位置の更新
+			_body.odm.update(_body.motorL.GetTachoCount(), _body.motorR.GetTachoCount());
 		}
 
 		public override void Exit()
@@ -23,6 +73,18 @@ namespace ETRobocon.StateMachine
 
 		public override TriggerID JudgeTransition()
 		{
+			if (_body.gyro.GetRapidChange ()) {
+				return TriggerID.DetectShock;
+			}
+			if (_body.touch.DetectReleased())
+			{
+				return TriggerID.TouchSensor;
+			}
+			if (CommandReceiveFlags.Instance.CheckCommandReceived(CommandID.Stop))
+			{
+				return TriggerID.StopCommand;
+			}
+
 			return TriggerID.NoTrigger;
 		}
 	}
