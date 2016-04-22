9fd2eb27154853d1d09df13d2087877ce464d367
Ubuntu <masanori@ETroboBuild.3llimveghh2e1kshrlodkeouoc.bx.internal.cloudapp.net>
2016-04-22 14:17:48 +0000

    #412 Add scripts for Doxygen

---
+++ scripts/updateDoxyfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Remove old files and directories
+OUTPUT_PATH=hrp2/workspace/html/
+rm -rf ${OUTPUT_PATH}
+
+# Execute doxygen
+doxygen ./scripts/workspace.Doxyfile
+
+echo "$1"
+
+if [ "${1}" = "Commit" ]
+then
+    # Commit files
+    git add ${OUTPUT_PATH}
+    git commit -am "#412 Update doxyfiles"
+fi
+
+exit 0
