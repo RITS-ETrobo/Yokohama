2eec9d9118f7a5a4bdfaca53edd5dfd6ee33d2fb
Masanori Ishigami <hermit00@hotmail.com>
2016-05-12 01:25:54 +0900

    #509 Add comments

--- scripts/make_project.sh
+++ scripts/make_project.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
+# This script is for making selected target directories
+
 if [ $# -ne 2 ]; then
+    echo "Illegal parameter" 1>&2
     echo "Counts of entered argument is $#." 1>&2
-    echo "For executing this script, need 2 arguments" 1>&2
+    echo "For executing this script, need 2 arguments." 1>&2
+    echo "The meaning of 1st parameter is the path of build target." 1>&2
+    echo "The meaning of 2nd parameter is build option." 1>&2
     exit 1
 fi
 
+# Remove that would be generated files and directories
 SCRIPT_REMOVE=../../../scripts/remove_file.sh
 ${SCRIPT_REMOVE} ../OBJ
 ${SCRIPT_REMOVE} app
 ${SCRIPT_REMOVE} hrp2
 ${SCRIPT_REMOVE} uImage
 
+# Execute make command to the selected project
 TARGET_PROJECT=$1
 echo "make $2=${TARGET_PROJECT}"
 make $2=${TARGET_PROJECT} 1>build.log 2>&1
 
+# Move the generated files and directories
 TARGET_PATH=../bin/$2/$1
 ${SCRIPT_REMOVE} ${TARGET_PATH}
 mkdir -p ${TARGET_PATH}
@@ -28,6 +36,10 @@ ${SCRIPT_MOVE} uImage ${TARGET_PATH}
 ${SCRIPT_MOVE} ../OBJ ${TARGET_PATH}
 
 # Check result
+# If result of make command is success, this script outputs "    SUCCEEDED".
+#   If parameter is "app", ${TARGET_PROJECT}.$2 exists under ${TARGET_PATH}.
+#   And, if parameter is not "app", hrp2 and uImage exist under ${TARGET_PATH}.
+# Otherwise, this script outputs "    FAILED".
 if [ "$2" == "app" ]; then
     if [ ! -e ${TARGET_PATH}/${TARGET_PROJECT}.$2 ]; then
         echo "    FAILED"
