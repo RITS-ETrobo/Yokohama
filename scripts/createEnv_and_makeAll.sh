7c91a0c1b8c7938dfdc78a37c45fb39a0196107b
Masanori Ishigami <masanori.ishigami@gmail.com>
2016-05-13 13:22:16 +0900

    #504 Add comments

--- scripts/createEnv_and_makeAll.sh
+++ scripts/createEnv_and_makeAll.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# Create environment
 ./scripts/create_env.sh
+
+# Remove binary files
 rm -rf hrp2/sdk/bin
+
+# Execute the make command
 ./scripts/make_all.sh app
 ./scripts/make_all.sh img
