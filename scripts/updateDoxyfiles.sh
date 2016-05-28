c898e902f4232469f1c70f6c4677fecccd231304
Masanori Ishigami <masanori.ishigami@gmail.com>
2016-05-28 14:41:16 +0000

    #506 Modify the comment in doxygen format

--- scripts/updateDoxyfiles.sh
+++ scripts/updateDoxyfiles.sh
@@ -7,12 +7,20 @@ OUTPUT_PATH=hrp2/sdk/html/
 rm -rf ${OUTPUT_PATH}
 
 # Execute doxygen
-doxygen ./scripts/workspace.Doxyfile
+echo "START: Updating the documents by Doxygen and Graphviz."
+echo ""
 
-echo "$1"
+doxygen ./scripts/workspace.Doxyfile 1>doxygen.log 2>&1
+mv doxygen.log ${OUTPUT_PATH}
+
+echo "END  : Updated the documents."
+echo "       Please open \"hrp/sdk/html/index.html\" in Web browser."
+echo ""
 
 if [ "${1}" = "Commit" ]
 then
+    echo "$1"
+
     # Commit files
     git add ${OUTPUT_PATH}
     git commit -am "#412 Update doxyfiles"
