2fc1ca0cb1f32bc2d064c18c8fbec8975229fff1
Masanori Ishigami <masanori.ishigami@gmail.com>
2016-05-31 20:57:48 +0900

    #551 Fix path

--- scripts/updateDoxyfiles.sh
+++ scripts/updateDoxyfiles.sh
@@ -7,13 +7,16 @@ OUTPUT_PATH=hrp2/sdk/html
 rm -rf ${OUTPUT_PATH}
 rm ${OUTPUT_PATH}/../doxygen.log
 
+projectRoot=`pwd`
+
 # Execute doxygen
 echo "START: Updating the documents by Doxygen and Graphviz."
 echo ""
 
 doxygen ./scripts/workspace.Doxyfile 1>doxygen.log 2>&1
-if [ ! -e ${OUTPUT_PATH}/../doxygen.log ]; then
+if [ ! -e ${projectRoot}/${OUTPUT_PATH}/../doxygen.log ]; then
     echo "ERROR: Perhaps, Doxygen and/or Graphviz are not installed."
+    cd ${projectRoot}
     exit 2
 fi
 
@@ -32,4 +35,5 @@ then
     git commit -am "#412 Update doxyfiles"
 fi
 
+cd ${projectRoot}
 exit 0
