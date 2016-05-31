673a4bc3b9e94b04c4beae83a59d0fb0dc93ce06
Masanori Ishigami <masanori.ishigami@gmail.com>
2016-05-31 20:50:04 +0900

    #551 Suspends execution when Doxygen is not installed

--- scripts/updateDoxyfiles.sh
+++ scripts/updateDoxyfiles.sh
@@ -11,7 +11,7 @@ echo "START: Updating the documents by Doxygen and Graphviz."
 echo ""
 
 doxygen ./scripts/workspace.Doxyfile 1>doxygen.log 2>&1
-if [ ! -e doxygen.log ]; then
+if [ ! -e ${OUTPUT_PATH}/../doxygen.log ]; then
     echo "ERROR: Perhaps, Doxygen and/or Graphviz are not installed."
     exit 2
 fi
