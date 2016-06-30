1bd806db618afc9cde1919da353a8145179178cc
Masanori Ishigami <masanori.ishigami@gmail.com>
2016-06-30 19:51:36 +0900

    #601 Fix merge conflict

--- hrp2/sdk/workspace/hackEV/RunningModule.h
+++ hrp2/sdk/workspace/hackEV/RunningModule.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * @file    RunningModule.h
  * @brief   This file has RunningModule
@@ -14,18 +13,3 @@
     extern void start_run();
     extern void run_withPID();
 #endif /* TOPPERS_MACRO_ONLY */
-=======
-/**
- * @file    RunningModule.h
- * @brief   This file has RunningModule
- */
-#pragma once
-
-//! ターゲット依存の定義
-#include "target_test.h"
-
-//  関数のプロトタイプ宣言
-extern void	initialize_run();
-extern void start_run(int power,int corveAngle);
-extern void run_withPID();
->>>>>>> hackev/cpp/develop
