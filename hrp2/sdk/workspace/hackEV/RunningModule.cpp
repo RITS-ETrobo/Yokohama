f9c14a46e099766dc67901dee49596f01a9960d1
masaYajima <masashi.yajima@jrits.ricoh.co.jp>
2016-08-01 20:24:45 +0900

    #710 ラインを見つけてライントレースできる位置まで移動

--- hrp2/sdk/workspace/hackEV/RunningModule.cpp
+++ hrp2/sdk/workspace/hackEV/RunningModule.cpp
@@ -20,6 +20,12 @@
 const float Pi = 3.14159265359F;
 //@}
 
+//! 白黒検知のバッファ
+const int COLOR_BUFFER = 5;
+
+//! カラーセンサーとタイヤの距離
+const float BETWEEN_COLOR_AND_WHEEL = 3.8F;
+
 //! \addtogroup 方向計算要素
 //@{
 //! 左モーターの回転数の合計
@@ -87,7 +93,10 @@ enum runPattern {
     SWITCH_SIDE_RIGHT,
     
     //! トレースするライン縁を変更する（ライン左側へ変更）
-    SWITCH_SIDE_LEFT
+    SWITCH_SIDE_LEFT,
+    
+    //! トレースするラインを探す
+    FIND_LINE
 };
 
 /**
@@ -270,6 +279,11 @@ const scenario_running run_scenario_test_right[] = {
     {20, 100.0F, -1, TRACE_STRAIGHT, true}
 };
 
+//! 検証用シナリオ(右側走行)
+const scenario_running run_scenario_test_find_Line[] = {
+    {30, 0.0F, -1, FIND_LINE, true}
+};
+
 /**
  * @brief   リセットしてからの指定したタイヤの走行距離を計算する
  * 
@@ -349,6 +363,141 @@ void initialize_wheel(){
 }
 
 /**
+ * @brief   ライントレースせずに直進走行
+ *
+ * @param   [in] 出力
+ * @return  なし
+*/
+void noTrace_Straight_Run(int power){
+    ev3_motor_set_power(EV3_MOTOR_LEFT, power);
+    ev3_motor_set_power(EV3_MOTOR_RIGHT, power);
+}
+
+/**
+ * @brief   走行体を位置にライントレースできる調整する ※調整後はライン右側
+ * 
+ * @param 走行体が動いた向き （ここはスタートからの角度でラインに並行になる向きにする予定）
+ * @return  なし
+ */
+void adjust_position_ToLine(int power){
+    //! 回転角、距離、角度を初期化
+    initialize_wheel();
+    
+    //! ラインまで直進
+    noTrace_Straight_Run(power);
+    for(;;){
+        //! 走行体のタイヤの真下にラインがくるまで
+        if(abs(getDistance(distance_running(EV3_MOTOR_RIGHT), distance_running(EV3_MOTOR_LEFT))) >= abs(BETWEEN_COLOR_AND_WHEEL)){
+            stop_run();
+            break;
+        }
+    }
+    
+    //! 回転角、距離、角度を初期化
+    initialize_wheel();
+    
+    //! 走行体を反対方向にその場回転させ、動いた分だけ戻す
+    pinWheel(-power);
+    bool onBlack = false;
+    for(;;){
+        //! センサーがライン縁に当たるまで回転
+        int colorValue = ev3_color_sensor_get_reflect(EV3_SENSOR_COLOR);
+        if(colorValue < (black + 5)){
+            onBlack = true;
+        }
+        if(colorValue > midpoint && onBlack){
+            stop_run();
+            break;
+        }
+    }
+}
+
+/**
+ * @brief   ラインを探す回転処理
+ * 
+ * @return  ラインが見つかったらtrue、見つからなかったらfalse
+ */
+bool rotate_for_findLine(int power){
+    float moveDirection = 0.0F;
+    for(;;){
+        //! 動いた角度を記録
+        moveDirection += getDirectionDelta(distance_running(EV3_MOTOR_LEFT), distance_running(EV3_MOTOR_RIGHT));
+        int colorValue = ev3_color_sensor_get_reflect(EV3_SENSOR_COLOR);
+        writeFloatLCD((float)colorValue);
+        if(colorValue < (black + COLOR_BUFFER)){
+            //! ラインを見つけたら止まる
+            stop_run();
+            return true;
+        }
+        else if(abs(moveDirection) >= 360){
+            //! 360度回転しても見つけられなかったら止まる
+            stop_run();
+            return false;
+        }
+    }
+}
+
+/**
+ * @brief   ラインを探す（走行体のTREDの2倍の範囲を探す)
+ *  最初にその場回転して探す。それでも見つけれない場合は片側回転して探す
+ * 
+ * @return  なし
+ */
+void find_Line(int power)
+{
+    //! 回転角、距離、角度を初期化
+    initialize_wheel();
+    
+    //! その場で360度回転して黒の線を見つける
+    pinWheel(power);
+    
+    bool findLine = false;
+    findLine = rotate_for_findLine(power);
+    
+    //! ラインを見つたら位置を調整して処理を終了
+    if(findLine){
+        //! 走行体の位置をライントレースできるように調整
+        adjust_position_ToLine(power);
+        return;
+    }
+    
+    //! その場回転では見つけれなかった場合、片側回転だけ回転で見つける(回転速度はその場回転の1.5倍)
+    ev3_motor_set_power(EV3_MOTOR_RIGHT, power*1.5);
+    findLine = rotate_for_findLine(power);
+    
+    //! ラインを見つたら位置を調整して処理を終了
+    if(findLine){
+        //! 走行体の位置をライントレースできるように調整
+        adjust_position_ToLine(power);
+        return;
+    }
+    
+    // moveDirection = 0.0F;
+    // for(;;){
+    //     //! 動いた角度を記録
+    //     moveDirection += getDirectionDelta(distance_running(EV3_MOTOR_LEFT), distance_running(EV3_MOTOR_RIGHT));
+        
+    //     int colorValue = ev3_color_sensor_get_reflect(EV3_SENSOR_COLOR);
+        
+    //     if(colorValue < (black + 5)){
+    //         //! ラインを見つけたら止まる
+    //         stop_run();
+            
+    //         //! 走行体の位置をライントレースできるように調整
+    //         adjust_position_ToLine(power, moveDirection);
+    //         findLine = true;
+    //         break;
+    //     }
+    //     else if(abs(moveDirection) >= 360){
+    //         //! 360度回転しても見つけられなかったら止まる
+    //         stop_run();
+    //         break;
+    //     }
+    // }
+    
+}
+
+/**
  * @brief   ラインの縁の変更処理
  * 
  * @enum LineSide targetSide 移動させたい縁
@@ -421,6 +570,8 @@ void change_LineSide(scenario_running scenario)
     } 
 }
 
+
+
 /**
  * @brief   シナリオに従って走る
  *
@@ -443,8 +594,7 @@ void run(scenario_running scenario)
 
         case NOTRACE_STRAIGHT:
             //! ライントレースせずに、直進走行する
-            ev3_motor_set_power(EV3_MOTOR_LEFT, scenario.power);
-            ev3_motor_set_power(EV3_MOTOR_RIGHT, scenario.power);
+            noTrace_Straight_Run(scenario.power);
             break;
             
         case SWITCH_SIDE_RIGHT:
@@ -464,6 +614,11 @@ void run(scenario_running scenario)
             ev3_motor_set_power(EV3_MOTOR_RIGHT, scenario.power);
             ev3_motor_stop(EV3_MOTOR_LEFT,true);
             break;
+            
+        case FIND_LINE:
+            //! ラインを探す
+            find_Line(scenario.power);
+            return;
         
         case TRACE_STRAIGHT_RIGHT:
         case TRACE_CURVE_RIGHT:
@@ -571,9 +726,9 @@ void start_run_test()
         }
     }
     
-    for (int index = 0; index < sizeof(run_scenario_test_UTern) / sizeof(run_scenario_test_UTern[0]); index++) {
+    for (int index = 0; index < sizeof(run_scenario_test_find_Line) / sizeof(run_scenario_test_find_Line[0]); index++) {
         //! シナリオが変わるたびに音を鳴らす
         ev3_speaker_play_tone(NOTE_E4, 100);
-        run(run_scenario_test_UTern[index]);
+        run(run_scenario_test_find_Line[index]);
     }
 }
