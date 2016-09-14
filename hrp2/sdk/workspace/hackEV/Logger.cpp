d339e23182710b091d6d5e496bef32a09d540310
Masanori Ishigami <masanori.ishigami@gmail.com>
2016-09-14 14:21:35 +0000

    #850 Reduce warning
    warning: format '%d' expects argument of type 'int', but argument 3 has type 'SYSTIM {aka long unsigned int}' [-Wformat=]

--- hrp2/sdk/workspace/hackEV/Logger.cpp
+++ hrp2/sdk/workspace/hackEV/Logger.cpp
@@ -235,7 +235,7 @@ void Logger::outputLog(bool doClosingLog /*= false*/)
             }
 
             char    logLine[64];
-            sprintf(logLine, "%d, %s, %s\r\n", it->logTime, getLogName(it->logType).c_str(), it->log);
+            sprintf(logLine, "%lu, %s, %s\r\n", it->logTime, getLogName(it->logType).c_str(), it->log);
             if (fputs(logLine, fpLog) == EOF) {
                 break;
             }
