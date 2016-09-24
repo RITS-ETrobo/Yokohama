89777cb97d92aad46a92ed3e795dc5a3f7e314cb
Masanori Ishigami <hermit00@hotmail.com>
2016-09-25 04:08:26 +0900

    #856 Update position
    1st commit

--- hrp2/sdk/workspace/hackEV/EV3Position.h
+++ hrp2/sdk/workspace/hackEV/EV3Position.h
@@ -27,6 +27,15 @@ typedef struct {
     float   direction;
 } DISTANCE_RECORD;
 
+//! @struct 位置を格納する構造体
+typedef struct {
+    //! X軸方向の位置[単位 : (実際の位置の場合)cm、(マップ上の位置の場合)ピクセル]
+    float   x;
+
+    //! Y軸方向の位置[単位 : (実際の位置の場合)cm、(マップ上の位置の場合)ピクセル]
+    float   y;
+} EV3_POSITION;
+
 //! Class for position
 class EV3Position
 {
@@ -37,10 +46,28 @@ public:
     virtual void add(DISTANCE_RECORD record);
     virtual float getSpeed(DISTANCE_RECORD *record);
     virtual float getDirection();
+    virtual bool getPosition(EV3_POSITION *positionREAL, EV3_POSITION *positionMAP, float *direction);
+    virtual void synchronizePosition(EV3_POSITION position, uint8_t updateType = 0);
+    virtual bool movePosition(EV3_POSITION *position, int distance_, float direction_, uint8_t updateType = 0, bool beCorrected = true);
+
+    //! \addtogroup correctPosition関数とmovePosition関数のupdateTypeに指定可能なビット
+    //@{
+    //! 実際の位置を補正する(CORRECT_POSITION_MAPと排他)
+    static const uint8_t    CORRECT_POSITION_REAL = (1 << 0);
+
+    //! マップ上の位置を補正する(CORRECT_POSITION_REALと排他)
+    static const uint8_t    CORRECT_POSITION_MAP = (1 << 1);
+
+    //! 向きを補正する
+    static const uint8_t    CORRECT_DIRECTION = (1 << 2);
+    //@}
 
 private:
     bool removeExceededTimeItem();
     void updateSpeed();
+    bool convertPostion(EV3_POSITION *positionREAL, EV3_POSITION *positionMAP, bool isExchangeReal2MAP = true);
+    bool isValidUpdateType(uint8_t updateType);
+    bool isValidPosition(EV3_POSITION *position, bool isPositionREAL = true, bool beCorrected = true);
 
     //! 平均速度算出に必要な時間[単位 : ms]
     SYSTIM  duration;
@@ -56,6 +83,15 @@ private:
 
     //! 初期化済み判定フラグ
     bool    initialized;
+
+    //! \addtogroup 現在位置
+    //@{
+    //! 実際の位置
+    EV3_POSITION    currentPositionREAL;
+
+    //! マップ上の位置
+    EV3_POSITION    currentPositionMAP;
+    //@}
 
     //! 平均速度算出の為の情報
     DISTANCE_RECORD record_speed;
