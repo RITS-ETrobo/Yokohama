04817af58abf37c237325a41f9446ee66a1244ff
masjinno <z00s600013@ZRFG031564.rfg.rfgad.ricoh.com>
2016-09-25 18:29:25 +0900

    #878 アームを水平にする関数を追加

--- hrp2/sdk/workspace/hackEV/ArmModule.cpp
+++ hrp2/sdk/workspace/hackEV/ArmModule.cpp
@@ -161,3 +161,19 @@ ER move_arm_throw_block(bool beforeSetDown = false, bool afterSetOriginalPositio
     return result;
 }
 
+/**
+ * @brief   アームを水平にする
+ *
+ * @return  ev3_motor_rotate() 参照
+ */
+ER move_arm_horizon()
+{
+    //! アームを水平にするときの角度[deg]。センサーが真下を向くときが0degree。
+    const int ARM_DEGREE_HORIZON = -31;
+    //! アームを水平にするときのアーム速度(=パワー)
+    const int ARM_SPEED_HORIZON = 30;
+
+    ER result = move_arm(ARM_DEGREE_HORIZON, ARM_SPEED_HORIZON, false);
+    return result;
+}
+
